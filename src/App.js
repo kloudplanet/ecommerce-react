bef667842b28f20562fd35dba292318cbe56b7c3
Dhaval <dhaval.7@outlook.com>
2021-05-23 00:07:33 +0530

    added header and navigation links

--- src/App.js
+++ src/App.js
@@ -5,16 +5,13 @@ import './App.css';
 
 import HomePage from './pages/homepage/Homepage.component.jsx'
 import ShopPage from './pages/shop/shop.component.jsx'
+import Header from './components/header/header.component.jsx'
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
